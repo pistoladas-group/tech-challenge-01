8b8d18ff499cabdd1b85451c5c7e1c521c9c83fe
Igor Moura <igor.moura254@hotmail.com>
2023-06-20 15:52:40 -0300

    refactor(api): Fixed incorrect use of using plus validations

--- src/api/TechBox.Api/Data/SqlServer/SqlServerStoredProcedureHandler.cs
+++ src/api/TechBox.Api/Data/SqlServer/SqlServerStoredProcedureHandler.cs
@@ -18,48 +18,47 @@ public sealed class SqlServerStoredProcedureHandler : IStoredProcedureHandler
 
     public async Task<IEnumerable<T1>> ExecuteListAsync<T1>(string procedureName, ListProcedureParameters procedureParameter, int? commandTimeout = null)
     {
-        using (_session)
-        {
-            var procedureResult = await _session.Connection.QueryAsync<T1>(
-                procedureName,
-                param: procedureParameter,
-                commandType: CommandType.StoredProcedure,
-                commandTimeout: commandTimeout ?? CommandTimeout);
+        if (!procedureName.StartsWith("SP_LST_")) throw new ArgumentException($"The procedure name {procedureName} is invalid for {nameof(ExecuteListAsync)}. It should start with SP_LST_<ProcName>.");
 
-            return procedureResult;
-        }
+        var procedureResult = await _session.Connection.QueryAsync<T1>(
+            procedureName,
+            param: procedureParameter,
+            commandType: CommandType.StoredProcedure,
+            commandTimeout: commandTimeout ?? CommandTimeout);
+
+        return procedureResult;
     }
 
     public async Task<T1> ExecuteGetAsync<T1>(string procedureName, GetProcedureParameters procedureParameter, int? commandTimeout = null)
     {
-        using (_session)
-        {
-            var procedureResult = await _session.Connection.QuerySingleAsync<T1>(
-                procedureName,
-                param: procedureParameter,
-                commandType: CommandType.StoredProcedure,
-                commandTimeout: commandTimeout ?? CommandTimeout);
+        if (!procedureName.StartsWith("SP_GET_")) throw new ArgumentException($"The procedure name {procedureName} is invalid for {nameof(ExecuteGetAsync)}. It should start with SP_GET_<ProcName>.");
 
-            return procedureResult;
-        }
+        var procedureResult = await _session.Connection.QuerySingleAsync<T1>(
+            procedureName,
+            param: procedureParameter,
+            commandType: CommandType.StoredProcedure,
+            commandTimeout: commandTimeout ?? CommandTimeout);
+
+        return procedureResult;
     }
 
     public async Task<int> ExecuteAddAsync(string procedureName, AddProcedureParameters procedureParameter, int? commandTimeout = null)
     {
-        using (_session)
-        {
-            var procedureResult = await _session.Connection.ExecuteAsync(
-                procedureName,
-                param: procedureParameter,
-                commandType: CommandType.StoredProcedure,
-                commandTimeout: commandTimeout ?? CommandTimeout);
+        if (!procedureName.StartsWith("SP_ADD_")) throw new ArgumentException($"The procedure name {procedureName} is invalid for {nameof(ExecuteAddAsync)}. It should start with SP_ADD_<ProcName>.");
 
-            return procedureResult;
-        }
+        var procedureResult = await _session.Connection.ExecuteAsync(
+            procedureName,
+            param: procedureParameter,
+            commandType: CommandType.StoredProcedure,
+            commandTimeout: commandTimeout ?? CommandTimeout);
+
+        return procedureResult;
     }
 
     public async Task<int> ExecuteUpdateAsync(string procedureName, UpdateProcedureParameters procedureParameter, int? commandTimeout = null)
     {
+        if (!procedureName.StartsWith("SP_UPD_")) throw new ArgumentException($"The procedure name {procedureName} is invalid for {nameof(ExecuteUpdateAsync)}. It should start with SP_UPD_<ProcName>.");
+
         var procedureResult = await _session.Connection.ExecuteAsync(
             procedureName,
             param: procedureParameter,
@@ -71,6 +70,8 @@ public sealed class SqlServerStoredProcedureHandler : IStoredProcedureHandler
 
     public async Task<int> ExecuteDeleteAsync(string procedureName, DeleteProcedureParameters procedureParameter, int? commandTimeout = null)
     {
+        if (!procedureName.StartsWith("SP_DEL_")) throw new ArgumentException($"The procedure name {procedureName} is invalid for {nameof(ExecuteDeleteAsync)}. It should start with SP_DEL_<ProcName>.");
+
         var procedureResult = await _session.Connection.ExecuteAsync(
             procedureName,
             param: procedureParameter,

0635e0c998febaa09b2898b3fdb2bb541be0642a
Igor Moura <igor.moura254@hotmail.com>
2023-06-20 15:55:46 -0300

    refactor(api+database): Adjusted the missing extension field

--- src/api/TechBox.Api/Data/Dto/AddFileDto.cs
+++ src/api/TechBox.Api/Data/Dto/AddFileDto.cs
@@ -5,7 +5,6 @@ namespace TechBox.Api.Data.Dto;
 public class AddFileDto : AddProcedureParameters
 {
     public string Name { get; set; }
-    public string Extension { get; set; }
     public int SizeInBytes { get; set; }
     public string? Url { get; set; }
     public ProcessStatusEnum ProcessStatusId { get; set; }
--- src/api/TechBox.Api/Data/Dto/GetProcedureParameters.cs
+++ src/api/TechBox.Api/Data/Dto/GetProcedureParameters.cs
@@ -6,6 +6,8 @@ public class GetProcedureParameters
 
     public GetProcedureParameters(Guid id)
     {
+        // TODO: Isso aqui não deixaria o Id vazio do mesmo jeito?
+        // Porq se não entrar no IF o default de Guid é um Guid.Empty mesmo
         if (id != Guid.Empty)
         {
             Id = id;
